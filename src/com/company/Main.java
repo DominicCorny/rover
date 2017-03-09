e2f85d8781f78b8e950fb9b6f828c833bc4e92a3
Tim <tim.vogel227@gmail.com>
2017-03-09 17:23:46 +0100

    refactored code, reliable reconnect if connection lost and conenction test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,47 +1,16 @@
 package com.company;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.Socket;
-import java.util.Scanner;
-//import com.pi4j.wiringpi.Gpio;import com.pi4j.wiringpi.SoftPwm;
 
 public class Main {
+    private static int lnCounter;
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
+     //   ServoControl speedServo = new ServoControl();
+     //   ServoControl steeringServo = new ServoControl(17);
 
-        Scanner scanner = new Scanner(System.in);
-        ServoControl speedServo= new ServoControl(17);
-        ServoControl steeringServo = new ServoControl();
-
-        while (true) {
-            try {
-                System.out.println("Try to connect");
-                Socket socket = new Socket("192.168.2.110", 4587);
-                InputStream inputStream = socket.getInputStream();
-                OutputStream outputstream = socket.getOutputStream();
-                byte[] buffer = new byte[2];
-                boolean running = true;
-                outputstream.write("PI Hello".getBytes());
-                System.out.println("PI Hello");
-                while (running) {
-                    if (inputStream.read(buffer) >= 2) {
-                        byte speed = buffer[0];
-                        byte steering = buffer[1];
-                        speedServo.SetValue(speed);
-                        steeringServo.SetValue(steering);
-                        System.out.println("New Speed: " + speed + " steering: " + steering + "\n");
-                        outputstream.write("PI OK".getBytes());
-                    }
-                }
-            }catch (IOException e){
-                System.out.println("Reconnecting to Server");
-            }
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        new ConnectionThread((speed, steering) -> {
+            System.out.println("Speed = " + speed + " steering = " + steering + '\t' + lnCounter++ % 10);
+      //      speedServo.SetValue(speed);
+      //      steeringServo.SetValue(steering);
+        }).start();
     }
 }
