ea090a1c474cb3c6c5decb3a568e5c4eb3e72996
DomiCore <kern.dominic@web.de>
2017-03-14 20:45:40 +0100

    erste tests mit dem Glattbahner

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,20 @@ import java.util.Date;
 public class Main {
     private static int lnCounter;
     public static void main(String[] args) {
-        TrackControl mainTrack = new TrackControl(17,22);
         SimpleDateFormat time = new SimpleDateFormat("HH:mm:ss");
+        ServoControl testSpeed = new ServoControl();
+        ServoControl testSteering = new ServoControl(23);
         new ConnectionThread((speed, steering) -> {
             System.out.println("Speed = " + speed + " steering = " + steering + '\t' + time.format(new Date()));
-            mainTrack.setTrack(speed,steering);
+            speed -= 50;
+            speed /= 4;
+            speed +=50;
+            testSpeed.setValue(speed);
+            steering -= 50;
+            steering *= -1;
+            steering /= 3;
+            steering += 50;
+            testSteering.setValue(steering);
         }).start();
     }
 }
