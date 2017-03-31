a0086b3467a27d52dc5b135e9867e18eedf0f8db
Tim <tim.vogel227@gmail.com>
2017-03-31 19:43:56 +0200

    use udp instead of tcp

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,23 @@
 package com.company;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Main {
-    private static int lnCounter;
-    public static void main(String[] args) {
-        TrackControl mainTrack = new TrackControl(17,22);
+
+    public static void main(String[] args) throws UnknownHostException {
         SimpleDateFormat time = new SimpleDateFormat("HH:mm:ss");
-        new ConnectionThread((speed, steering) -> {
+        InetSocketAddress address = new InetSocketAddress(InetAddress.getByName("127.0.0.1"), 3841);
+
+        //TrackControl mainTrack = new TrackControl(17,22);
+
+        new ConnectionThread(address, (speed, steering) -> {
             System.out.println("Speed = " + speed + " steering = " + steering + '\t' + time.format(new Date()));
-            mainTrack.setTrack(speed,steering);
+            //mainTrack.setTrack(speed,steering);
+
         }).start();
     }
 }
