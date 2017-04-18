b8658d97d0eb6d514f801f2f38dcc8d8c987884f
DomiCore <kern.dominic@web.de>
2017-04-18 17:40:20 +0200

    Anfang mit Fotos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
@@ -10,22 +9,22 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) throws UnknownHostException {
-        try {
+        /*try {
             VideoStream cam = new VideoStream();
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             System.out.println("fatal problem in Main");
             e.printStackTrace();
-        }
+        }*/
         SimpleDateFormat time = new SimpleDateFormat("HH:mm:ss");
         InetSocketAddress address = new InetSocketAddress(InetAddress.getByName("roverpi.cf"), 3841);
 
-        //TrackControl mainTrack = new TrackControl(17,22);
+        TrackControl mainTrack = new TrackControl(17,22);
 
         new ConnectionThread(address, (speed, steering) -> {
             System.out.println("Speed = " + speed + " steering = " + steering + '\t' + time.format(new Date()));
-            //mainTrack.setTrack(speed,steering);
+            mainTrack.setTrack(speed,steering);
         }).start();
     }
 }
